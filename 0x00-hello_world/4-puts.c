14fabf4a8f02028f0b9c75b179e0320e23f104c3
Hikmah97 <olanipekunhikmahkikelomo@gmail.com>
2022-06-30 22:00:38 +0100

    Test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-
- /**
-  * main - Entry point
-  * Return: Always 0 (success)
-  */
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
