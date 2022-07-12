f2272bbca9f546435a75026fb40bf85a3c907abb
Hikmah97 <olanipekunhikmahkikelomo@gmail.com>
2022-07-12 06:31:24 -0700

    i love you betty

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,35 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * main - program that generates random valid
- * passwords for the program 101-crackme
- *
- * Return: Always 0 (Success)
- */
+#define GRAPH_MIN 33
+#define GRAPH_MAX 126
 
+/**
+ * main - generate valid passwords for 101-crackme
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int x, sum, y;
-	int pass[100];
+int sum = 2772;
+char c;
 
-	sum = 0;
+srand(time(NULL));
 
-	srand(time(NULL))
+while (sum > GRAPH_MAX)
+{
+c = rand() % (GRAPH_MAX - GRAPH_MIN) + GRAPH_MIN;
 
-	for (x = 0; x < 100; x++)
-	{
-		pass[x] = rand() % 78;
-		sum += (pass[x] + '0');
-		putchar(pass[x] + '0');
-		if ((2772 - sum) - '0' < 78)
-		{
-			y = 2772 - sum - '0';
-			sum += y;
-			putchar(y + '0');
-			break;
-		}
-	}
+sum -= c;
 
-	return (0);
+if (sum < GRAPH_MIN)
+{
+c -= (GRAPH_MIN - sum);
+sum = GRAPH_MIN;
+}
+putchar(c);
+
+}
+putchar(sum);
+
+return (0);
+
 }
